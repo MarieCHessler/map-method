88b35d9bde1eecd7b528015ea1953ee6b82d670d
MarieCHessler <marie@hessler.se>
2022-03-13 09:05:13 +0000

    Initial commit

--- map.js
+++ map.js
@@ -5,15 +5,31 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = []; // Create a new array with all nums multiplied by 2
+for (let num of nums) {
+  results.push(num * 2)
+};
 
+console.log(results)
 
 // Using map()
+const multiplyByTwo = function (num) {
+  return num * 2
+};
+
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults)
 
 
 // Simplified w/ map()
+const simplified = nums.map(function (num) { return num * 2}); // Place the function inside the parentheses
+console.log(simplified)
 
 
 // Simplfied w/ map() + arrow function
+const arrow = nums.map(num => num * 2); // Only one parameter in function so no parantheses needed, return value also simple so no parantheses or curly braces needed
+console.log(arrow);
 
 
 // With objects:
@@ -37,3 +53,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]); // For each student function returns student name and student id
+console.log(studentsWithIds) // Shows an array of arrays containing student.name, student.id
